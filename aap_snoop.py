7705b2e8d9355bd259429156e4b29905f6337b88
Leszek Nowak <saginata@gmail.com>
2020-05-03 19:26:54 +0100

    CRC checking, logging to file

--- aap_snoop.py
+++ aap_snoop.py
@@ -3,11 +3,15 @@ import time
 import datetime
 import keyboard
 ser = serial.Serial('COM6', 57600, timeout=0.001)
-ser.write(b'\xFF\xFF\x55\x00\x03\xAD\xFF\x55\x04\x03\xCD\xFF\xFF\xFF\xFF\xFF\x55\x04\x03\xCD\xFF\x55\x04')
+logfile = open("messagelog.txt", "a")
+# ser.write(b'\xFF\xFF\x55\x03\x00\x01\x04\xF8\xFF\x55\x06\x03\xCD\xFF\xFF\xFF\xFF\xFF\x55\x02\x03\xCD\xFF\x55\x04')
+ser.write(b'\xFF\x55\x02\x00\x03\xFC\xFF\x55\x03\x04\x00\x1E\xDB\xFF\x55\x03\x04\x00\x1C\xDD\xFF\x55\xFF\x55\xFF\x55\xFF\x55')
 message = []
 
+
 # temporary loop, will make it infinite with breaks later
-for k in range(23):
+# for k in range(23):
+while 1:
     # idle if buffer empty
     while ser.in_waiting == 0:
         time.sleep(0.001)
@@ -15,8 +19,49 @@ for k in range(23):
     rx = ser.read().hex()
     message.append(rx)
 
-    # detect header of next message
-    if len(message) >= 2 and message[-2] == 'ff' and message[-1] == '55':
+    mes_size = 0
+    mes_ok = False
+
+    # record the expected size if message long enough to have one
+    if len(message) >= 3:
+        mes_size = int(message[2], 16)+1
+
+    # if message ended correctly
+    if len(message)-3 == mes_size:
+
+        # CSUM
+        csum = 0
+        for k in message[2:-1]:
+            csum = csum + int(k, 16)
+        csum = csum % 256
+        csum = 256-csum
+        #print(csum, end=' should be ')
+        #print(int(message[-1], 16))
+
+        if csum == int(message[-1], 16):
+            mes_ok = True
+
+    if mes_ok:
+
+        # timestamp
+        ts_str = str(datetime.datetime.now().time())
+        print('[' + ts_str + '  OK ] ', end=' ')
+
+        # message
+        for k in message:
+            print(k.upper(), end=' ')
+        print('')
+
+        # logmessage
+        # datetime.datetime.fromtimestamp(time.time())
+        logfile.write(str(time.time())+" ")
+        for k in message:
+            logfile.write(k)
+        logfile.write("\n")
+        message = []
+
+    # if not ended correctly
+    elif len(message) >= 2 and message[-2] == 'ff' and message[-1] == '55':
 
         # remove it from the current message
         message = message[:-2]
@@ -26,12 +71,20 @@ for k in range(23):
 
             # timestamp
             ts_str = str(datetime.datetime.now().time())
-            print('[' + ts_str + '] ', end=' ')
+            print('[' + ts_str + ' NOK ] ', end=' ')
 
             # message
             for k in message:
                 print(k.upper(), end=' ')
             print('')
+
+            # logfile
+            logfile.write(str(time.time())+" ")
+            for k in message:
+                logfile.write(k)
+            logfile.write("\n")
+
+            message = []
 
         # put the header at the beginning of next message
         message = []
@@ -40,6 +93,7 @@ for k in range(23):
 
 # empty the buffer
 ser.read(100)
+logfile.close()
 ser.close()
 
 
