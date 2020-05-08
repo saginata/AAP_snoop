7dd86582dfe9013de0df14c658d34e21b8e5f9d3
Leszek Nowak <saginata@gmail.com>
2020-05-08 22:01:27 +0100

    Should now show up in the car

--- aap_impersonate.py
+++ aap_impersonate.py
@@ -3,6 +3,30 @@ import time
 import datetime
 
 
+def send_response(respStr):
+    print('[' + ts_str + ']', end=' ')
+    print("Responding ", end=' ')
+    print(responseStr)
+
+    logfile.write(str(time.time()) +
+                  " Responding " + responseStr + "\n")
+
+    # serial port wants bytes
+    response = bytes.fromhex(responseStr)
+    ser.write(response)
+
+
+def checksum(checkStr):
+    # function will strip out the header
+    checkList = [checkStr[i:i+2] for i in range(0, len(checkStr), 2)]
+    csum = 0
+    for k in checkList[2:]:
+        csum = csum + int(k, 16)
+    csum = csum % 256
+    csum = 256-csum
+    return f"{csum:0{2}x}"
+
+
 ser = serial.Serial('COM6', 57600, timeout=0.001)
 
 logfile = open("messagelog.txt", "a")
@@ -16,7 +40,7 @@ translModes = []
 respModes = []
 responses = []
 
-playlists = ["Main", "Now Playing", "Test"]
+playlists = ["Main", "Now Playing", "Test", "90s Music"]
 songs = []
 
 songs.append({'Title': 'Piosenka 1', 'Artist': 'Artysta 1',
@@ -24,9 +48,9 @@ songs.append({'Title': 'Piosenka 1', 'Artist': 'Artysta 1',
 songs.append({'Title': 'Piosenka 2', 'Artist': 'Artysta 2',
               'Album': 'Plyta 2', 'Length': 666})
 songs.append({'Title': 'Piosenka 3', 'Artist': 'Artysta 3',
-              'Album': 'Plyta 3', 'Length': 420})
+              'Album': 'Plyta 2', 'Length': 420})
 songs.append({'Title': 'Piosenka 4', 'Artist': 'Artysta 4',
-              'Album': 'Plyta 4', 'Length': 69})
+              'Album': 'Plyta 1', 'Length': 69})
 
 
 # load command database
@@ -54,6 +78,7 @@ while 1:
     rx = ser.read().hex()
     message.append(rx)
 
+    # region check for correctness
     # record the expected size if message long enough to have one
     if len(message) >= 3:
         mes_size = int(message[2], 16)+1
@@ -70,7 +95,7 @@ while 1:
 
         if csum == int(message[-1], 16):
             mes_ok = True
-
+    # endregion
     if mes_ok:
 
         # region print and log
@@ -139,27 +164,113 @@ while 1:
         # endregion
 
         # region Respond
+        # ignoring these messages
         if respModes[foundIndex] == '0':
             print('[' + ts_str + ']', end=' ')
             print("Not responding")
             logfile.write(str(time.time()) + " Not responding\n")
+
+        # easy responses, no thinking, copy from database
         elif respModes[foundIndex] == '1':
             responseStr = responses[foundIndex]
+            send_response(responseStr)
 
-            print('[' + ts_str + ']', end=' ')
-            print("Responding ", end=' ')
-            print(responseStr)
-
-            logfile.write(str(time.time()) +
-                          " Responding " + responseStr + "\n")
-
-            response = bytes.fromhex(responseStr)
-            ser.write(response)
-
+        # more complicated, need to figure out the response here
         elif respModes[foundIndex] == '2':
-            abc = 1
+
+            # RequestLingoProtocolVersion
+            if commandToFind == '000f':
+                if payload[0] == '00':
+                    responseStr = 'ff55050010000109e1'
+                elif payload[0] == '04':
+                    responseStr = 'ff5505001004010ed8'
+                elif payload[0] == '0a':
+                    responseStr = 'ff550500100a0102de'
+                else:
+                    responseStr = ''
+                send_response(responseStr)
+
+            # GetCountOfType
+            elif commandToFind == '040018':
+                # fixed part of the message
+                responseStr = 'ff5507040019'
+                countStr = ''
+                # count playlists
+                if payload[0] == '01':
+                    # number of playlists padded to 4 bytes
+                    countStr = f"{len(playlists):0{8}x}"
+                # count albums
+                elif payload[0] == '03':
+                    Albums = []
+                    for t in songs:
+                        if t['Album'] not in Albums:
+                            Albums.append(t['Album'])
+
+                    countStr = f"{len(Albums):0{8}x}"
+                # count songs
+                elif payload[0] == '05':
+                    countStr = f"{len(songs):0{8}x}"
+                else:
+                    countStr = f"{0:0{8}x}"
+                # checksum
+                responseStr = responseStr + countStr
+
+                # add checksum
+                checksumStr = checksum(responseStr)
+                responseStr = responseStr + checksumStr
+
+                send_response(responseStr)
+
+            # GetNamesRange
+            elif commandToFind == '04001a':
+                # fixed part of the message
+                responseStr = 'ff550704001b'
+
+                startIndex = int(''.join(i for i in payload[1:5]), 16)
+                count = int(''.join(i for i in payload[5:9]), 16)
+
+                stringsToSend = []
+                indexes = []
+
+                # list playlists
+                if payload[0] == '01':
+                    for i in range(count):
+                        indexes.append(i+startIndex)
+                        stringsToSend.append(playlists[i+startIndex])
+                # list albums
+                elif payload[0] == '03':
+                    Albums = []
+                    for t in songs:
+                        if t['Album'] not in Albums:
+                            Albums.append(t['Album'])
+                    for i in range(count):
+                        indexes.append(i+startIndex)
+                        stringsToSend.append(Albums[i+startIndex])
+                # list tracks
+                elif payload[0] == '05':
+                    for i in range(count):
+                        indexes.append(i+startIndex)
+                        stringsToSend.append(songs[i+startIndex]['Title'])
+
+                for i in range(len(stringsToSend)):
+                    responseStr = 'ff550704001b'
+                    indexStr = f"{indexes[i]:0{8}x}"
+                    responseStr = responseStr + indexStr
+                    for c in stringsToSend[i]:
+                        # convert each char to hex string
+                        tempstr = f"{ord(c):0{2}x}"
+                        responseStr = responseStr + tempstr
+                    responseStr = responseStr + '00'
+                    respLen = int((len(responseStr)-6)/2)
+                    lenStr = f"{respLen:0{2}x}"
+                    responseStr = responseStr[:4] + lenStr + responseStr[6:]
+                    checksumStr = checksum(responseStr)
+                    responseStr = responseStr + checksumStr
+                    send_response(responseStr)
 
         # endregion
+        # ready for next message
+
         message = []
 
     # if not ended correctly
@@ -192,6 +303,7 @@ while 1:
         message = []
         message.append('ff')
         message.append('55')
+
 ser.read(100)
 ser.close()
 logfile.close()
